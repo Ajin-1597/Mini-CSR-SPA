93f2f9e762446ff6b931825001c0562260ba638e
NDH <nick@ndhrones.com>
2023-10-23 13:33:33 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Mini CSR-SPA
+
+This is a simple Deno Single-Page-Application server -- **./server.ts**.     
+
+I've provided a minimal app with Client-Side-Routing to test this server -- **./index.html**.    
+
+```
+deno run -A server.ts
+```
---
+++ server.ts
@@ -0,0 +1,43 @@
+import { serveFile } from "https://deno.land/std@0.203.0/http/file_server.ts"
+import { openWebsite } from "https://raw.githubusercontent.com/nhrones/Browser/master/browser.ts"
+
+const PORT = 8080
+
+Deno.serve({ port: PORT }, async (request: Request): Promise<Response> => {
+
+   const { pathname } = new URL(request.url);
+   console.log('serving: ', pathname)
+
+   // root request
+   if (pathname === '/') {
+      return await serveRoot(200, './index.html')
+   }
+
+   // file request
+   if (!isRouteRequest(pathname)) {
+      return  await serveFile(request, `.${pathname}`)
+   }
+
+   // A route request -- serve index.html with 301 status
+   return await serveRoot(301, './index.html')
+})
+
+function isRouteRequest(path: string) {
+   //@ts-ignore ?
+   return path.split('/').pop().indexOf('.') === -1 ? true : false;
+}
+
+async function serveRoot(statusCode: number, fullPath: string) {
+   const body =  await Deno.readTextFile(fullPath)
+   // Create appropriate headers - we don't want to cache this 
+   const headers = new Headers([
+      ["content-type", "text/html; charset=utf-8"],
+      ["pragma", "no-cache"],
+      ["expires", "0"],
+      ["Cache-Control", "max-age=0, no-cache, no-store, must-revalidate"]
+   ])
+   return new Response(body, { status: statusCode, headers: headers });
+}
+
+// Trigger browser start
+await openWebsite(`http://localhost:${PORT}`)
